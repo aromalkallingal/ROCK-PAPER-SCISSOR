c5a1cdcacec664f37d2372388a2cd8c205dcd6da
aromalkallingal <aromalkallingal5@gmail.com>
2022-07-21 16:43:47 +0530

    Added buttons for playing game

--- index.js
+++ index.js
@@ -4,54 +4,137 @@ const computerPlay = () => {
     return thing.toLowerCase();
 }
 
+
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorButton = document.querySelector('.scissor');
+const outcomeDiv = document.querySelector('.outcome');
+const playerScoreSpan = document.querySelector('.player-score');
+const computerScoreSpan = document.querySelector('.comp-score');
+
+
+
+
 let compScore = 0
 let playerScore = 0
 
 
 let playRound = (computerSelection,playerLower) => {
     if (computerSelection == playerLower){
-        return "tie";
+        const p = document.createElement('p');
+        p.innerText = "You Tied!"
+        outcomeDiv.appendChild(p)
     }else if(computerSelection == 'rock' && playerLower == 'paper' ){
-        playerScore++
-        return "you win"
+        playerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You win";
+        outcomeDiv.appendChild(p);
     }else if(computerSelection == 'rock' && playerLower == 'scissor' ){
         compScore++
-        return "computer win"
+        const p = document.createElement('p');
+        p.innerText = "Computer win";
+        outcomeDiv.appendChild(p);
     }else if(computerSelection == 'paper' && playerLower == 'scissor' ){
         playerScore++
-        return "you win"
+        const p = document.createElement('p');
+        p.innerText = "You win";
+        outcomeDiv.appendChild(p);
     }else if(computerSelection == 'paper' && playerLower == 'rock' ){
         compScore++
-        return "computer win"
+        const p = document.createElement('p');
+        p.innerText = "Computer win";
+        outcomeDiv.appendChild(p);
     }else if(computerSelection == 'scissor' && playerLower == 'rock' ){
         playerScore++
-        return "you win"
+        const p = document.createElement('p');
+        p.innerText = "You win";
+        outcomeDiv.appendChild(p);
     }else if(computerSelection == 'scissor' && playerLower == 'paper' ){
         compScore++
-        return "computer win"
+        const p = document.createElement('p');
+        p.innerText = "Computer win";
+        outcomeDiv.appendChild(p);
     }
 }
 
-const game =  () => {
-    for(let i = 0; i < 5; i++){
-        const computerSelection = computerPlay()
-        let playerSelection = prompt("enter rock, paper, scissors")
-        let playerLower = playerSelection.toLowerCase()
-        playRound(computerSelection, playerLower)
+
+
+const checkForWinner = (playerScore, compScore) => {
+    if (playerScore === 5) {
+        const h2 = document.createElement('h2');
+        h2.classList.add('player-won');
+        h2.innerText = "You Won the game";
+        outcomeDiv.append(h2);
+        const rstBtn = document.createElement('button');
+        rstBtn.innerText = "reset";
+        outcomeDiv.append(rstBtn);
+        rstBtn.addEventListener ("click", function() {
+            location.reload();});
+        rockButton.disabled = true;
+        scissorButton.disabled = true;
+        paperButton.disabled = true;
+        
     }
+
+    if (compScore === 5) {
+        const h2 = document.createElement('h2');
+        h2.classList.add('computer-won');
+        h2.innerText = "Computer Won the game";
+        outcomeDiv.append(h2);
+        const rstBtn = document.createElement('button');
+        rstBtn.innerText = "reset";
+        outcomeDiv.append(rstBtn);
+        rstBtn.addEventListener ("click", function() {
+            location.reload();});
+        rockButton.disabled = true;
+        scissorButton.disabled = true;
+        paperButton.disabled = true;
+        
+    }
+ 
+   
 }
 
-game()
-console.log('player score = ' + playerScore)
-console.log('computer score = ' + compScore)
 
-if (playerScore > compScore){
-    console.log("you beat the computer")
-}else if(compScore > playerScore){
-    console.log('You lost')
-}else{
-    console.log("it's a tie")
+
+
+
+const updateScore = (playerScore, compScore) => {
+    playerScoreSpan.innerText = `Player Score: ${playerScore}`
+    computerScoreSpan.innerText = `Computer Score: ${compScore}`
 }
+
+
+
+rockButton.addEventListener('click', function() {
+    const computerSelection = computerPlay();
+    const playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore, compScore);
+    updateScore(playerScore, compScore);
+});
+
+
+
+paperButton.addEventListener('click', function() {
+    const computerSelection = computerPlay();
+    const playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore, compScore);
+    updateScore(playerScore, compScore);
+});
+
+
+
+scissorButton.addEventListener('click', function() {
+    const computerSelection = computerPlay();
+    const playerSelection = "scissor";
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore, compScore);
+    updateScore(playerScore, compScore);
+});
+
 
 
 
